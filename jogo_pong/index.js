c2ebe7e3d68fe973e803ec37e03debe3366f647d
Caio-Vinicius-Dias <caiodiasprofissional@gmail.com>
2025-08-01 07:59:09 -0300

    Atualização do index.js

--- jogo_pong/index.js
+++ jogo_pong/index.js
@@ -12,20 +12,20 @@ const paddleWidth = 18,
     netWidth = 5,
     netColor = "WHITE";
 
-// Draw net on canvas
+
 function drawNet() {
     for (let i = 0; i <= canvas.height; i += 15) {
         drawRect(canvas.width / 2 - netWidth / 2, i, netWidth, 10, netColor);
     }
 }
 
-// Draw rectangle on canvas
+
 function drawRect(x, y, width, height, color) {
     context.fillStyle = color;
     context.fillRect(x, y, width, height);
 }
 
-// Draw a circle on canvas
+
 function drawCircle(x, y, radius, color) {
     context.fillStyle = color;
     context.beginPath();
@@ -34,7 +34,7 @@ function drawCircle(x, y, radius, color) {
     context.fill();
 }
 
-// Draw text on canvas
+
 function drawText(text, x, y, color, fontSize = 60, fontWeight = 'bold', font = "Courier New") {
     context.fillStyle = color;
     context.font = `${fontWeight} ${fontSize}px ${font}`;
@@ -42,25 +42,25 @@ function drawText(text, x, y, color, fontSize = 60, fontWeight = 'bold', font = 
     context.fillText(text, x, y);
 }
 
-// Create a paddle object
+
 function createPaddle(x, y, width, height, color) {
     return { x, y, width, height, color, score: 0 };
 }
 
-// Create a ball object
+
 function createBall(x, y, radius, velocityX, velocityY, color) {
     return { x, y, radius, velocityX, velocityY, color, speed: initialBallSpeed };
 }
 
-// Define user and computer paddle objects
+
 const user = createPaddle(0, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, "WHITE");
 
 const com = createPaddle(canvas.width - paddleWidth, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, "WHITE");
 
-// Define ball object
+
 const ball = createBall(canvas.width / 2, canvas.height / 2, ballRadius, initialBallSpeed, initialBallSpeed, "WHITE");
 
-// Update user paddle position based on mouse movement
+
 canvas.addEventListener('mousemove', movePaddle);
 
 function movePaddle(event) {
@@ -68,14 +68,14 @@ function movePaddle(event) {
     user.y = event.clientY - rect.top - user.height / 2;
 }
 
-// Check for collision between ball and paddle
+
 function collision(b, p) {
     return (
         b.x + b.radius > p.x && b.x - b.radius < p.x + p.width && b.y + b.radius > p.y && b.y - b.radius < p.y + p.height
     );
 }
 
-// Reset ball position and velocity
+
 function resetBall() {
     ball.x = canvas.width / 2;
     ball.y = Math.random() * (canvas.height - ball.radius * 2) + ball.radius;
@@ -83,9 +83,9 @@ function resetBall() {
     ball.speed = initialBallSpeed;
 }
 
-// Update game logic
+
 function update() {
-    // Check for score and reset ball if necessary
+    
     if (ball.x - ball.radius < 0) {
         com.score++;
         resetBall();
@@ -94,19 +94,19 @@ function update() {
         resetBall();
     }
 
-    // Update ball position
+
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
 
-    // Update computer paddle position based on ball position
+    
     com.y += (ball.y - (com.y + com.height / 2)) * 0.1;
 
-    // Top and bottom walls
+    
     if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
         ball.velocityY = -ball.velocityY;
     }
 
-    // Determine which paddle is begin hit by the ball and handle collision
+    
     let player = ball.x + ball.radius < canvas.width / 2 ? user : com;
     if (collision(ball, player)) {
         const collidePoint = ball.y - (player.y + player.height / 2);
@@ -115,7 +115,7 @@ function update() {
         ball.velocityX = direction * ball.speed * Math.cos(collisionAngle);
         ball.velocityY = ball.speed * Math.sin(collisionAngle);
 
-        // Increase ball speed and limit to max speed
+        
         ball.speed += 0.4;
         if (ball.speed > maxBallSpeed) {
             ball.speed = maxBallSpeed;
@@ -123,32 +123,32 @@ function update() {
     }
 }
 
-// Render game on canvas
+
 function render() {
-    // Clear canvas with black screen
+    
     drawRect(0, 0, canvas.width, canvas.height, "BLACK");
     drawNet();
 
-    // Draw scores
+   
     drawText(user.score, canvas.width / 4, canvas.height / 2, "GRAY", 120, 'bold');
     drawText(com.score, (3 * canvas.width) / 4, canvas.height / 2, "GRAY", 120, 'bold');
 
-    // Draw paddles
+    
     drawRect(user.x, user.y, user.width, user.height, user.color);
     drawRect(com.x, com.y, com.width, com.height, com.color);
 
-    // Draw ball
+   
     drawCircle(ball.x, ball.y, ball.radius, ball.color);
 
 }
 
 
-// Run game loop
+
 function gameLoop() {
     update();
     render();
 }
 
-// Set gameLoop to run at 60 frame per second
+
 const framePerSec = 60;
 setInterval(gameLoop, 1000 / framePerSec);
